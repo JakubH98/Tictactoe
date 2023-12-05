26c649294d7c2f607a0b9673681b44d0b036a347
Byndum <Mikael.List-Jensen@hotmail.dk>
2023-12-05 09:21:28 +0100

    getPlayers() -> setPlayerNames() + public player variables

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
+    public String player1;
+    public String player2;
 
     public static void main(String[] args) {
 
@@ -6,9 +10,12 @@ public class Main {
 
 
     }
-    public void getPlayers() {
-
+    public void setPlayerNames() {
+        Scanner input = new Scanner(System.in);
         System.out.println("Name of player 1: ");
+        player1 = input.next();
+        System.out.println("name of player 2: ");
+        player2 = input.next();
 
     }
 }
