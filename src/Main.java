0c866acbdbab08f8e6042efd047844e3002a7fb0
Byndum <Mikael.List-Jensen@hotmail.dk>
2023-12-05 10:00:10 +0100

    printBoard fixed

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static char[][] board = {{'_', '_', '_'}, {'_', '_', '_'}, {'_', '_', '_'}};
 
     public static void main(String[] args) {
+
         //boardet i form af et 2D array.
         printBoard(board);
 
     }
+    public static void updateBoard() {
+        int indexX;
+        int indexY;
+        Scanner input = new Scanner(System.in);
+        indexY = input.nextInt();
+        indexX = input.nextInt();
+    }
+
+
 
     public static void printBoard(char[][] board) {
         for (int i = 0; i < board.length; i++) {
