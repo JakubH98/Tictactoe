b8cafad5065b8357dc5b679d0aa57ccaea9e1e2e
JakubH98 <jakubsmail8@gmail.com>
2023-12-05 09:49:32 +0100

    Method that prints the board.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static char[][] board = {{'_', '_', '_'}, {'_', '_', '_'}, {'_', '_', '_'}};
 
+    public static void main(String[] args) {
         //boardet i form af et 2D array.
-        char[][] board = {{'_','_','_'},{'_','_','_'}, {'_','_','_'}};
+        printBoard(board);
 
     }
-    public void getPlayers() {
 
-        System.out.println("Name of player 1: ");
-
+    public static void printBoard(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
