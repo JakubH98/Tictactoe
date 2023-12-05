119b2ee2a71a466e4f198a004beb12ccaaa744e8
Byndum <Mikael.List-Jensen@hotmail.dk>
2023-12-05 09:24:49 +0100

    made public strings static

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-    public String player1;
-    public String player2;
+    public static String player1;
+    public static String player2;
 
     public static void main(String[] args) {
-
+        setPlayerNames();
         //boardet i form af et 2D array.
 
 
     }
-    public void setPlayerNames() {
+    public static void setPlayerNames() {
         Scanner input = new Scanner(System.in);
         System.out.println("Name of player 1: ");
-        player1 = input.next();
+        player1= input.next();
         System.out.println("name of player 2: ");
         player2 = input.next();
 
