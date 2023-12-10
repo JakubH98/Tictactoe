cb5480ef34970ae6e0cf8fa073c32da67da58c6e
JakubH98 <jakubsmail8@gmail.com>
2023-12-10 20:19:18 +0100

    Place move method

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,26 @@ public class Main {
 
     }
 
-    public static void printBoard(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
+    // allows the user to place a Move on the board
+    private static void placeMove(char[][] board, String position, char symbol) {
+        switch (position) {
+            case "1" -> board[0][0] = symbol;
+            case "2" -> board[0][1] = symbol;
+            case "3" -> board[0][2] = symbol;
+            case "4" -> board[1][0] = symbol;
+            case "5" -> board[1][1] = symbol;
+            case "6" -> board[1][2] = symbol;
+            case "7" -> board[2][0] = symbol;
+            case "8" -> board[2][1] = symbol;
+            case "9" -> board[2][2] = symbol;
+            default -> System.out.println("Numbers 0-9 only.");
         }
+    }
+
+    // the board
+    private static void printBoard(char[][] board) {
+        System.out.println(board[0][0] + " " +  board[0][1] + " " +  board[0][2] );
+        System.out.println(board[1][0] + " " +  board[1][1] + " " +  board[1][2] );
+        System.out.println(board[2][0] + " " +  board[2][1] + " " +  board[2][2] );
     }
 }
